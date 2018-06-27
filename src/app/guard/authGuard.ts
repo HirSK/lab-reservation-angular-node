f5bbc7b18d872d6ebd414ffe1f2882881e79debb
Hirunika karunathilaka <hirukarunathilaka@gmail.com>
2018-06-27 16:23:58 +0530

    secured routes with canActivate

---
+++ src/app/guard/authGuard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { UserService } from '../components/services/UserService';
+import { CanActivate } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
+
+
+@Injectable({providedIn: 'root'})
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private router: Router,
+    private userService: UserService
+  ) { }
+
+  canActivate() {
+    if (this.userService.loggedIn()) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+
+}
